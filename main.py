f5579d71ef97d2705c119f7e2c2513ca17299f08
Larry Le <lle114@student.hbuhsd.edu>
2022-03-12 13:51:03 -0800

    Heroku Support

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from nextcord import Interaction, interactions
 async def test_if_working(context):
   await context.reply(f"I'm working <@{context.author.id}>")
 
-@bot.slash_command(name = "spin", description = "Spin", guild_ids = [586680405302968321])
+@bot.slash_command(name = "spin", description = "Spin", guild_ids = [586680405302968321, 949110139531722802, 834242088391540796])
 async def spin_command(interaction: Interaction):
 
   await interaction.response.send_message("https://tenor.com/view/computer-computador-spin-spinning-3d-gif-23612820")
