b35d6f85d78a174d6af6e3ee8717320851291dc0
Larry Le <lle114@student.hbuhsd.edu>
2022-03-12 13:48:18 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ from nextcord import Interaction, interactions
 
 # Functions ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+@bot.command(name = "test", description = "Test if the bot is working")
+async def test_if_working(context):
+  await context.reply(f"I'm working <@{context.author.id}>")
+
 @bot.slash_command(name = "spin", description = "Spin", guild_ids = [586680405302968321])
 async def spin_command(interaction: Interaction):
 
