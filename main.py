46373514473e24e11d30d2ec0abbbf043af5f4c5
Larry Le <lle114@student.hbuhsd.edu>
2022-03-25 19:32:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,17 @@ async def on_ready():
 async def test_if_working(context):
   await context.reply(f"I'm working <@{context.author.id}>")
 
+# Start a giveaway with a specified time and random select a user id
+@bot.command(name = "giveaway", description = "Giveway?? It's pretty self-explanitory")
+async def give_now(context):
+
+  # Get the channel that context was sent in (context grabs all data)
+  # context.message specially grabs the message that the user inputs
+  channelName = bot.get_channel(context.message.channel.id)
+  
+  joinMessage = await channelName.send("React to join the giveaway!")
+  await joinMessage.add_reaction("🎉")
+
 # Slash command that returns a picture of a computer GIF
 @bot.slash_command(name = "spin", description = "Spin", guild_ids = [586680405302968321, 949110139531722802, 834242088391540796])
 async def spin_command(interaction: Interaction):
@@ -37,4 +48,4 @@ async def spin_command(interaction: Interaction):
 
 # MainSetup ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-bot.run(discordToken)
+bot.run("ODg2MTE0ODQxOTY4OTE0NDQ1.YTw4wQ.LmY9onKEPJQn4jTOrpKaGepm4Ok")
