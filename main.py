bae425a25a5234bc812fb3d1530ed3941702afef
Larry Le <lle114@student.hbuhsd.edu>
2022-03-25 20:37:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,20 @@
-# Variables ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Variables ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
 import nextcord
-from nextcord import Interaction
+from nextcord import interactions
 from nextcord.ext import commands
 
-from datetime import datetime
-import os, asyncio
-discordToken = os.getenv("TOKEN")
-bot = commands.Bot(command_prefix = "uh ")
+import os
+bot = commands.bot(command_prefix = "hey ")
 
-# Functions ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Functions ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-# Display the date since the bot was last updated
-@bot.event
-async def on_ready():
-    print(f"{bot.user} has turned itself on!")
-    startTime = datetime.timestamp(datetime.now())
+@bot.command(name = "working?")
+async def test_working(context, *arg):
+  context.reply(f"uh {arg}")
 
-    while True:
-        await asyncio.sleep(5)
-        currentTime = datetime.timestamp(datetime.now())
-        elapsedTime = round((float(currentTime) - float(startTime)))
 
-        await bot.change_presence(activity = nextcord.Game(name = f"for {round((elapsedTime / 3600), 2)} Hours"))
+# MainSetup ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-# Test command using the command prefix
-@bot.command(name = "test", description = "Test if the bot is working")
-async def test_if_working(context):
-  await context.reply(f"I'm working <@{context.author.id}>")
-
-# Slash command that returns a picture of a computer GIF
-@bot.slash_command(name = "spin", description = "Spin", guild_ids = [586680405302968321, 949110139531722802, 834242088391540796])
-async def spin_command(interaction: Interaction):
-
-  await interaction.response.send_message("https://tenor.com/view/computer-computador-spin-spinning-3d-gif-23612820")
-
-# MainSetup ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-bot.run("ODg2MTE0ODQxOTY4OTE0NDQ1.YTw4wQ.LmY9onKEPJQn4jTOrpKaGepm4Ok")
+# This token is found on Heroku's configure var section
+bot.run(os.getenv("TOKEN"))
