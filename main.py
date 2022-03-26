82a90a93122c741731f09052b84f830b018ad760
Larry Le <lle114@student.hbuhsd.edu>
2022-03-25 21:57:27 -0700

    hey set time...
    Syntax: hey set time [minutes] [hours] [days]

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 # Variables ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
+import datetime
+from datetime import datetime, timedelta
+
 import nextcord
 from nextcord import interactions
 from nextcord.ext import commands
 
-import os
-bot = commands.Bot(command_prefix = "daddy ")
+import os, asyncio
+bot = commands.Bot(command_prefix = "hey ")
 
 # Functions ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
-@bot.command(name = "say")
-async def test_working(context, *arg):
-  await context.reply(f"uh {' '.join(arg)}")
+# Next Goals: Add a confirm time option, anti-break system, a more specific time like 7:00 PM
+@bot.command(name = "set")
+async def set_timer(context, timer, minutes = 0, hours = 0, days = 0):
+  
+  # If the user inputs 'hey set timer...' then
+  if timer == "timer":
+    # Get the current time that the user sends the message
+    currentTime = datetime.now()
+    print(f"starting at {currentTime}")
 
+    futureTime = currentTime + timedelta(minutes = minutes, hours = hours, days = days)
+    await context.send(f"Alright. I have a timer set for **{futureTime}.**")
+
+    while (currentTime < futureTime) == True:
+      await asyncio.sleep(30)
+
+      currentTime = datetime.now()
+      print(currentTime)
+
+  authorUser = context.message.author.id
+  await context.send(f"This is your reminder to do something <@{authorUser}>!!")
 
 # MainSetup ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
 # This token is found on Heroku's configure var section
-bot.run(os.getenv("TOKEN"))
+#bot.run(os.getenv("TOKEN"))
+bot.run("ODg2MTE0ODQxOTY4OTE0NDQ1.YTw4wQ.Vv1V6yjgdMyIanrRX8niCgCnUgI")
